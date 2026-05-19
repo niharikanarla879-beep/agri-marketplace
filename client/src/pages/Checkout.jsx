128e1173077b0ddb22bfae91e2b707e2e8756880
niharikanarla <niharikanarla879@gmail.com>
2026-05-19 14:28:04 +0530

    fixed backend url

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -81,7 +81,7 @@ export default function Checkout() {
     };
 
     const response = await axios.post(
-      "https://agri-marketplace-zliu.onrender.com/api/orders/place",
+      "https://agri-marketplace-backend.onrender.com/api/orders/place",
       orderData
     );
 
